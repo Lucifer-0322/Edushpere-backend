8f9ae33394428884d7e5269dcc60ec3f9e10d04b
Ali Raza <roylucifer003@gmail.com>
2026-06-30 11:23:24 +0500

    fix: allow admin to delete any course

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const { register, login, forgotPassword } = require('../controllers/authController');
 
+router.post('/forgot-password', forgotPassword);
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 
